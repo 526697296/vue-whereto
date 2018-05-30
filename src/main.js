30d1afa91090549893c7bade4bf537476f20eab8
526697296 <526697296@qq.com>
2018-05-30 14:18:50 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 移动端300ms延迟
+import fastClick from 'fastclick'
+// 样式初始化
+import './assets/styles/reset.css'
+// 移动端1px问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
