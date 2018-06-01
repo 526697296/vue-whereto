5caad0c46f4086ea6fee50d392921bdcadaccbd4
526697296 <526697296@qq.com>
2018-06-01 20:36:37 +0800

    apl.vue逻辑

--- src/pages/city/components/Apl.vue
+++ src/pages/city/components/Apl.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="cityapl">
         <ul class="list">
-            <li @click="handLetterClick" class="item" v-for="(item, key) in cities" :key="key">{{key}}</li>
+            <li :ref="item" @click="handLetterClick" @touchstart="handTouchStart" @touchmove="handTouchmove" @touchend="handTouchend" class="item" v-for="item in letters" :key="item">{{item}}</li>
         </ul>
     </div>
 </template>
@@ -12,10 +12,20 @@ export default {
             type:Object
         }
     },
+    computed:{
+        letters () {
+            const letters = [];
+            for (let i in this.cities) {
+                letters.push(i)
+            }
+            return letters
+        }
+    },
     name:'cityapl',
     data () {
         return {
-
+            touchstarts: false,
+            timer:null
         }
     },
     methods: {
@@ -23,6 +33,29 @@ export default {
         handLetterClick (e) {
             // console.log(e.target.innerText);
             this.$emit('change', e.target.innerText)
+        },
+        handTouchStart () {
+            this.touchstarts = true
+        },
+        handTouchmove (e) {
+            if(this.touchstarts) {
+                // 节流
+                if (this.timer) {
+                    clearTimeout(this.timer);
+                }
+                this.timer = setTimeout(() => {
+                    const startY = this.$refs['A'][0].offsetTop;
+                    const touchY = e.touches[0].clientY - 110;
+                    const index = Math.floor((touchY - startY) / 20);
+                    if (index >= 0 && index < this.letters.length) {
+                        this.$emit('change',this.letters[index])
+                    }
+                },16)
+                
+            }
+        },
+        handTouchend () {
+            this.touchstarts = false
         }
     }
 }
